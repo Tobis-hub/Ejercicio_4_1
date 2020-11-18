527762ecfb536edff921eb7fca1f48bb72d0a17e
Tobina <Tobina@DESKTOP-00KFRPU>
2020-11-18 01:09:26 +0100

    añadido en metodo leer un mensaje prueba 2

--- ejercicio_4_1/src/AleatorioEmple.java
+++ ejercicio_4_1/src/AleatorioEmple.java
@@ -17,6 +17,7 @@ public class AleatorioEmple {
 		String nom;
 		double salario;
 		int pos, num, dep;
+		System.out.println("prueba 2");
 		if (file.length() > 0) {
 			pos = 0;
 			System.out.println(" ------------------------------------------");
